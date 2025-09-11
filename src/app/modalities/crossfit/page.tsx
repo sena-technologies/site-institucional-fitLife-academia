f6cecda9a091e7d2699039241301784739422175
igorpaes21 <irsp2121@hmail.com>
2025-09-11 20:14:54 -0300

    fix: adiciona página de CrossFit

---
+++ src/app/modalities/crossfit/page.tsx
@@ -0,0 +1,8 @@
+export default function CrossfitPage() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <h1>CrossFit</h1>
+      <p>Página em construção...</p>
+    </div>
+  );
+}
